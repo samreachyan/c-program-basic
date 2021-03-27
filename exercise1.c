ec3e503d4e720faaae0311d17e6930d11528d8d4
samreachyan <samreachyan@gmail.com>
2021-03-27 14:58:57 +0700

    c variable

--- exercise1.c
+++ exercise1.c
@@ -3,10 +3,10 @@
 void main() {
     int x = 500, y = 200;
     char s = 'b';
-    float c = (float) x / y;
+    double c = (double) x / y;
     printf("This is char = %c", s);
     printf("\na * b = %d", x * y);
-    printf("\na / b = %.2f", c);
+    printf("\na / b = %.2lf", c);
     printf("\na - b = %d", x - y);
     printf("\na + b = %d \n", x + y);
 }
